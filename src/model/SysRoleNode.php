49b363a4be80eee39fa4aafd46be394829089d44
诗无尽头i <svipchao@qq.com>
2024-08-28 21:02:04 +0800

    修复权限

--- src/model/SysRoleNode.php
+++ src/model/SysRoleNode.php
@@ -15,7 +15,7 @@ class SysRoleNode extends Model
         return $this->where('role_id', 'in', function ($query) use ($user_id) {
             // $userDeptIds = array_column(SysDept::mk()->getUserDept($user_id), 'id');
             // $userDeptIds = array_column(SysDept::mk()->getUserDept($user_id), 'id');
-            $userDeptIds = UserService::instance()->getUserDeptIds($user_id);
+            $userDeptIds = UserService::instance()->getUserDeptIds($user_id, true);
             return $query->table('sys_dept_role')->field('role_id')->where('dept_id', 'in', $userDeptIds);
         })->where('role_id', 'not in', function ($query) {
             return $query->table('sys_role')->where('status', 0)->field('id');
